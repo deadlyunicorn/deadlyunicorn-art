33cc98056b7a6d83e3c1111d7aea1da7043265a4
deadlyunicorn <retroalex1008@gmail.com>
2023-09-29 04:47:30 +0300

    patched security vulnerability

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,25 @@
-import { cookies } from "next/headers"
+import { User } from "next-auth";
+import { headers } from "next/headers"
 import { NextRequest, NextResponse } from "next/server";
 
-export const middleware = ( request: NextRequest ) => {
+export const middleware = async( request: NextRequest ) => {
 
-  const authCookie = cookies().get('next-auth.session-token');
+  const session:session = await fetch(`${process.env.serverURL}/api/auth/session`, {
+    headers:headers()
+  })
+  .then( async( res ) => await res.json() );
 
+  const loggedIn = Object.keys(session).length > 0? true :false;
   const pathname = request.nextUrl.pathname;
 
-  if ( pathname != "/admin/login" && !( authCookie && authCookie.value ) ){
+  if ( pathname != "/admin/login" && !loggedIn ){
     return NextResponse.redirect(`${process.env.serverURL}/admin/login`);
   }
-
-
-  
 
 }
 
 export const config = {
   matcher : ["/admin/:path*"]
 }
+
+type session = {} | User;
